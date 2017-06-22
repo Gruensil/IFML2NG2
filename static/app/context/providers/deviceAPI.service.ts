be127a2d4c2466a61f6634f015049765bb2483fc
Silas Grün (Surface) <gruensil@mail.upb,de>
2017-06-22 16:20:53 +0200

    Edited model names again

--- static/app/context/providers/deviceAPI.service.ts
+++ static/app/context/providers/deviceAPI.service.ts
@@ -1,10 +1,12 @@
 // PROTECTED REGION ID deviceAPI ENABLED START
-    private acceleartionAvg = 1;
-    private i = 0;
+    private acceleartionAvg = 0.5;      // helper for moving average with magic starting value
+    // private i = 0;                    // helper for debugging
 // PROTECTED REGION END 
  
 
 // PROTECTED REGION ID constructor ENABLED START
+
+        //Ambientlight not implemented!!
         // window.addEventListener('devicelight', event => {
 
         //     if (event.value > 300) {
@@ -21,10 +23,11 @@
         // Updates Movement information for vertical movement
         window.addEventListener("devicemotion", event => {
 
-            // x,y,z are the accelerations on different axis
-            // all combined have a value in still position of ~13
-            // this is due acceleration of gravtiy
-            // if the device is shaken or moved the value rises
+            /*  x,y,z are the accelerations on different axis.
+                All combined have a value in still position of ~13.
+                This is due acceleration of gravtiy.
+                If the device is shaken or moved the value rises.                
+            */
             
             var x = event.accelerationIncludingGravity.x;
             var y = event.accelerationIncludingGravity.y;
@@ -32,21 +35,27 @@
 
 			var w = y+z+x;
 
+            /*  If the combined acceleration rises above a level
+                a moving average is increased. All the used magic values and threshold
+                are eperimental and turned out to work fine
+            */
+
+            this.acceleartionAvg = this.acceleartionAvg*24;
             if(w > 14 || w < 8.5){
-                this.acceleartionAvg = 20;
+                this.acceleartionAvg += 100;
             }
-            this.acceleartionAvg = this.acceleartionAvg/2;
+            this.acceleartionAvg = this.acceleartionAvg/25;
 
-            if(this.i == 100){
-                console.log(this.acceleartionAvg);
-                this.i=0;
-            }else{
-                this.i++;
-            }
+            // if(this.i == 100){
+            //     console.log(this.acceleartionAvg);
+            //     this.i=0;
+            // }else{
+            //     this.i++;
+            // }
 
-            if(this.acceleartionAvg >= 2){
+            if(this.acceleartionAvg >= 1){
                 this.movement = 2;
-            }else if(this.acceleartionAvg >= 1){
+            }else if(this.acceleartionAvg >= 0.5){
                 this.movement = 1;
             }else{
                 this.movement = 0;
